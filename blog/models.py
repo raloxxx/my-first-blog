f09f1b85af5fb0a833b4aaf413aed5db72c14640
juan <juan.15121001@gmail.com>
2018-08-23 16:46:22 -0500

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
@@ -11,5 +12,6 @@ class Post(models.Model):
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
+
 	def __str__(self):
 		return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Post
+
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 def post_list(request):
-    template = loader.get_template('blog/post_list.html')
-    context = {
-        'name': "juan manuel",
-    }
-    return HttpResponse(template.render(context, request))
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+#def post_list(request):
+#   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#   template = loader.get_template('blog/post_list.html')
+#   context = {
+#      'name': "juan manuel",
+#   }
+#  return HttpResponse(template.render(context, request))
