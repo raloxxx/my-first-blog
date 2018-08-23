3ddca04c91c30696d5fdf2f758637eb9324437f0
juan <juan.15121001@gmail.com>
2018-08-23 15:49:08 -0500

    Cambie el HTML para la página.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def post_list(request):
+    template = loader.get_template('blog/post_list.html')
+    context = {
+        'name': "juan manuel",
+    }
+    return HttpResponse(template.render(context, request))
