9ee44eb74e84d267f8ba726ead501cc26bd1bbcc
juan <juan.15121001@gmail.com>
2018-08-27 02:55:31 -0500

    additional files and changes for Heroku

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-	url('', include('blog.urls')),
-    url('admin/', admin.site.urls)
+    url('', include('blog.urls')),
+    url('admin/', admin.site.urls),
+    url('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    url('accounts/logout/', auth_views.LoginView.as_view(), name='logout', kwargs={'next_page': '/'}),
     
 ]
